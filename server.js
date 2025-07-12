f22791c90d40bd374091c54969b0b2659053437b
mignrod <mignrod@gmail.com>
2025-07-12 17:15:09 -0400

    adding lint

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require('express');
 
 const mongodb = require('./db/database');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-
 mongodb.initDb((err) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        app.listen(port, () => { console.log(`Database is listening and node Runnign on port ${port}`) });
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node Runnign on port ${port}`);
+    });
+  }
 });
-
-
-
-
